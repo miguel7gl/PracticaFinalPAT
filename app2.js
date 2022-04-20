d6e00dddbf2926a89241bc7b8c1e6e44f95d5441
miguel7gl <201911481@alu.comillas.edu>
2022-04-20 18:21:05 +0200

    20/04/2022

--- app2.js
+++ app2.js
@@ -1,81 +1,45 @@
 //Boton Buscar
-let button= document.querySelector('.submit');
+let button= document.getElementById('submit');
 
 //URLs para la búsqueda de datos
 const urlPrecios = 'https://api.esios.ree.es/indicators/';
+const url2 = "https://api.esios.ree.es/archives?date=2015-09-15T23:59:59+00:00";
 const token = "67ff456a8376fbbb16a69c06b925535675ccc05b153c04f28219a66013a810c5";
 
-/*
-const headers = {
-  Accept: application/json, application/vnd.esios-api-v1+json,
-  Content-Type: application/json,
-  Host: api.esios.ree.es,
-  Authorization: token="67ff456a8376fbbb16a69c06b925535675ccc05b153c04f28219a66013a810c5"
-};
-*/
+const url = 'https://api.esios.ree.es/indicators/1001'
+const hdrs = {'Accept':'application/json; application/vnd.esios-api-v2+json','Content-Type':'application/json','Host':'api.esios.ree.es','Authorization':'Token token="67ff456a8376fbbb16a69c06b925535675ccc05b153c04f28219a66013a810c5"'}
 
 //Definimos lo que hace el boton cuando es pulsado
 button.addEventListener('click', function(name){
-  fetch(urlPrecios, {Authorization: Token token='67ff456a8376fbbb16a69c06b925535675ccc05b153c04f28219a66013a810c5'})
+  fetch(url, {method: "GET", headers: hdrs})
   .then(response => response.json())
   .then(data => {
 
+    console.log(typeof data.indicator.values[0].datetime);
     console.log(data);
+    console.log(data.indicator.id);
+
+    let datos = '';
+    let hora = '';
+    let precio = '';
 
     //Creamos una variable body donde almacenamos los datos
+    let fecha = data.indicator.values[0].datetime.substring(0,10);
     let body = '';
-    for (let x in data){
-      
+
+    for(i=0; i<data.indicator.values.length; i+=5)
+    {      
       //Imprimimos los datos en consola para comprobar que son cogidos correctamente
-      console.log(x+": "+data[x]['price']);
+      console.log(data.indicator.values[i].datetime.substring(0,10)+" ("+data.indicator.values[i].datetime.substring(11,19)+"): "+data.indicator.values[i].value);
       
       //Añadimos los datos a la variable body
-      body += `<tr><td>${x}</td><td>${data[x]['price']}</td></tr>`;
+      body += `<tr><td>${data.indicator.values[i].datetime.substring(11,19)}</td><td>${data.indicator.values[i].value}</td></tr>`;
     }
 
     //Mostramos la variable body en la página
+    document.getElementById('prueba').innerHTML = fecha;
     document.getElementById('datos').innerHTML = body;
     
-    /*
-    console.log('Hola');
-    let body = '';
-
-    for (let i=0; i<data.length; i++){
-      body += i;
-      console.log(i);
-    }
-    console.log('Adios');
-
-    document.getElementById('prueba').innerHTML = body;
-    */
-
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var myChart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday",
-        ],
-        datasets: [
-          {
-            label: "work load",
-            data: [2, 9, 3, 17, 6, 3, 7],
-            backgroundColor: "rgba(153,205,1,0.6)",
-          },
-          {
-            label: "free hours",
-            data: [2, 2, 5, 5, 2, 1, 10],
-            backgroundColor: "rgba(155,153,10,0.6)",
-          },
-        ],
-      },
-    });
   })
   .catch(err => alert("Error en el acceso a los datos"));
   })

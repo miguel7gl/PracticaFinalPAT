6535f95f6cb10e8ef8229b260328d212da9c21f0
IgnacioHernándezBas <201904604@alu.comillas.edu>
2022-04-20 18:21:59 +0200

    Creación de carpeta con SpringBoot

---
+++ PractFinal/src/main/java/com/Spring/PractFinal/PractFinalApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.PractFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractFinalApplication.class, args);
+	}
+
+}

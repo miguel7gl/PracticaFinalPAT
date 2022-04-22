3279d65ff7d1fe33157fd95795d87a0fa8b4e557
miguel7gl <201911481@alu.comillas.edu>
2022-04-22 15:15:42 +0200

    god

--- PractFinal/src/main/resources/static/Api.js
+++ PractFinal/src/main/resources/static/Api.js
@@ -1,22 +1,41 @@
 //Boton Buscar
-let button= document.getElementById('submit');
+let buttonActual= document.getElementById('submitActual');
+let buttonIntervalo= document.getElementById('submitIntervalo');
 
 
+/*
 //URLs para la búsqueda de datos
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const urlPrecios = 'https://api.esios.ree.es/indicators/';
 const url2 = "https://api.esios.ree.es/archives?date=2015-09-15T23:59:59+00:00";
+const url = 'https://api.esios.ree.es/indicators/1001'
+const urlprueba = "https://api.esios.ree.es/indicators/1001?start_date=01-01-2014T00:00:00+02:00&end_date=01-01-2014T23:50:00+02:00&geo_agg=sum&geo_ids&time_trunc=hour&time_agg=&locale=es";
+
+*/
+
+//Token necesario para la aplicacion
 const token = "67ff456a8376fbbb16a69c06b925535675ccc05b153c04f28219a66013a810c5";
 
-const url = 'https://api.esios.ree.es/indicators/1001'
+//Variable que almacena los headers necesarios para el fetch
 const hdrs = {'mode': 'no-cors', 'Accept':'application/json; application/vnd.esios-api-v2+json','Content-Type':'application/json','Host':'api.esios.ree.es','Authorization':'Token token="67ff456a8376fbbb16a69c06b925535675ccc05b153c04f28219a66013a810c5"'}
 
-//const urlprueba = "https://api.esios.ree.es/indicators/1001?start_date=01-01-2014T00:00:00+02:00&end_date=01-01-2014T23:50:00+02:00&geo_agg=sum&geo_ids&time_trunc=hour&time_agg=&locale=es";
-const urlintervalo = "https://api.esios.ree.es/indicators/1001?start_date=21-04-2022T00:00:00+02:00&end_date=21-04-2022T23:50:00+02:00";
+//Obtenemos el día actual
+let date = new Date();
+let diaActual = String(date.getDate()).padStart(2, '0') + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + date.getFullYear();
 
-//Definimos lo que hace el boton cuando es pulsado
-button.addEventListener('click', function(name){
-  fetch(urlintervalo, {method: "GET", headers: hdrs})
+
+
+
+
+//CUANDO PULSAMOS EL BOTON DEL DIA ACTUAL
+
+buttonActual.addEventListener('click', function(name){
+
+  //url al que haremos fetch
+  const urlDiaActual = "https://api.esios.ree.es/indicators/1001?start_date="+diaActual+"T00:00:00+02:00&end_date="+diaActual+"T23:50:00+02:00"
+
+
+  fetch(urlDiaActual, {method: "GET", headers: hdrs})
   .then(response => response.json())
   .then(data => {
 
@@ -27,6 +46,11 @@ button.addEventListener('click', function(name){
     let horasGrafico = []; //Horas para el grafico
     let datosGrafico = []; //Precios para el grafico
     let colorBarras = []; //Colores para el grafico
+
+    //Obtenemos el día actual
+    let date = new Date();
+    let output = String(date.getDate()).padStart(2, '0') + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + date.getFullYear();
+    console.log(output);
 
     //Variable que almacena la fecha actual
     let fecha = data.indicator.values[0].datetime.substring(0,10);
@@ -44,7 +68,7 @@ button.addEventListener('click', function(name){
     }
 
     let sum = datosGrafico.reduce((previous, current) => current += previous);
-    let mediaDatos = sum / datosGrafico.length;
+    let mediaDatos = (sum / datosGrafico.length).toFixed(2);
 
     let mediaDatosGrafico = [];
     for(i=0; i<datosGrafico.length; i++)
@@ -55,9 +79,6 @@ button.addEventListener('click', function(name){
     //Mostramos la variable body en la página
     document.getElementById('prueba').innerHTML = fecha;
     document.getElementById('datos').innerHTML = body;
-
-    //Grafica
-    let ctx = document.getElementById("myChart").getContext("2d");
     
     //Obtenemos el dato minimo y su indice
     let datoMin = Math.min.apply(null, datosGrafico);
@@ -87,20 +108,31 @@ button.addEventListener('click', function(name){
       }
     }
 
+    //Grafica
+    let ctx = document.getElementById("myChartDiaActual").getContext("2d");
+
+    const graficoEjes = {
+      labels: horasGrafico,
+      datasets: [{
+        type: 'bar',
+        label: 'Precio por horas',
+        data: datosGrafico,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: colorBarras
+      }, {
+        type: 'line',
+        label: 'Precio medio',
+        data: mediaDatosGrafico,
+        fill: false,
+        borderColor: 'rgb(54, 162, 235)'
+      }]
+    };
+
     
     //Aqui creamos la grafica
     let myChart = new Chart(ctx, {
       type: "bar",
-      data: {
-        labels: horasGrafico,
-        datasets: [
-          {
-            label: "Precio por horas",
-            data: datosGrafico,
-           backgroundColor: colorBarras
-          },
-        ],
-      },
+      data: graficoEjes,
       options: {
         scales: {
             xAxes: [{
@@ -118,3 +150,128 @@ button.addEventListener('click', function(name){
   .catch(() => alert("Error en el acceso a los datos"));
   })
 
+
+  //CUANDO PULSAS EL BOTON DE INTERVALO
+
+  buttonIntervalo.addEventListener('click', function(name){
+
+    //Detectamos las fechas introducidas y creamos el url al que haremos fetch
+    let fechaInicio = (document.getElementById('fechaInicio').value);
+    let fechaFin = (document.getElementById('fechaFin').value);
+
+    if (fechaInicio=="" || fechaFin=="")
+    {
+      fechaIncio = "";
+      fechaFin = "";
+    }
+
+    const urlIntervalo = "https://api.esios.ree.es/indicators/1001?start_date="+fechaInicio+"T00:00:00+02:00&end_date="+fechaFin+"T23:50:00+02:00";
+
+    fetch(urlIntervalo, {method: "GET", headers: hdrs})
+    .then(response => response.json())
+    .then(data => {
+  
+      console.log(urlIntervalo);
+
+      console.log(data);
+  
+      //Creamos una variable body donde almacenamos los datos
+      let body = '';
+      let horasGrafico = []; //Horas para el grafico
+      let datosGrafico = []; //Precios para el grafico
+      let colorBarras = []; //Colores para el grafico
+  
+      for(i=0; i<data.indicator.values.length; i+=5)
+      {      
+        //Imprimimos los datos en consola para comprobar que son cogidos correctamente
+        console.log(data.indicator.values[i].datetime.substring(0,10)+" ("+data.indicator.values[i].datetime.substring(11,16)+"): "+data.indicator.values[i].value);
+        
+        //Anadimos los datos a la variable body
+        body += `<tr><td>${data.indicator.values[i].datetime.substring(11,16)}</td><td>${data.indicator.values[i].value}</td><td>${(data.indicator.values[i].value/10).toFixed(2)}</td></tr>`;
+        
+        datosGrafico.push(data.indicator.values[i].value); //Array con todos los precios para hacer la grafica
+        horasGrafico.push(data.indicator.values[i].datetime.substring(0,10)+": "+data.indicator.values[i].datetime.substring(11,16)); //Array con todas las horas
+      }
+  
+      let sum = datosGrafico.reduce((previous, current) => current += previous);
+      let mediaDatos = (sum / datosGrafico.length).toFixed(2);
+  
+      let mediaDatosGrafico = [];
+      for(i=0; i<datosGrafico.length; i++)
+      {
+        mediaDatosGrafico.push(mediaDatos);
+      }
+  
+      //Mostramos la variable body en la página
+      document.getElementById('prueba').innerHTML = "";
+      document.getElementById('datos').innerHTML = body;
+  
+      //Obtenemos el dato minimo y su indice
+      let datoMin = Math.min.apply(null, datosGrafico);
+      let indexMin = datosGrafico.indexOf(datoMin);
+  
+      //Obtenemos el vlor maximo y su indice
+      let datoMax = Math.max.apply(null, datosGrafico);
+      let indexMax = datosGrafico.indexOf(datoMax);
+  
+      //Creamos un array con los colores de las barras:
+      //  - Si coincide con el indice minimo se pone en verde
+      //  - Si coincide con el indice maximo se pone en rojo
+      //  - Si no coincide con ninguno se pone de amarillo
+      for(i=0; i<data.indicator.values.length; i++)
+      {
+        if(i==indexMin)
+        {
+          colorBarras.push("rgba(112, 255, 51,0.6)");
+        }
+        else if(i==indexMax)
+        {
+          colorBarras.push("rgba(255, 87, 51,0.6)");
+        }
+        else
+        {
+          colorBarras.push("rgba(255, 195, 0,0.6)");
+        }
+      }
+      
+      //Grafica
+      let ctx = document.getElementById("myChartIntervalo").getContext("2d");
+
+      const graficoEjes = {
+        labels: horasGrafico,
+        datasets: [{
+          type: 'bar',
+          label: 'Precio por horas',
+          data: datosGrafico,
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: colorBarras
+        }, {
+          type: 'line',
+          label: 'Precio medio',
+          data: mediaDatosGrafico,
+          fill: false,
+          borderColor: 'rgb(54, 162, 235)'
+        }]
+      };
+  
+      
+      //Aqui creamos la grafica
+      let myChart = new Chart(ctx, {
+        type: "bar",
+        data: graficoEjes,
+        options: {
+          scales: {
+              xAxes: [{
+                  ticks: {
+                      autoSkip: false,
+                      maxRotation: 90,
+                      minRotation: 90
+                  }
+              }]
+          }
+      }  
+      }); 
+  
+    })
+    .catch(() => alert("Error en el acceso a los datos"));
+    })

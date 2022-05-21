5fab8deaea6fc0633530685fd8a0106c132c0a9f
javierjg001 <56931870+javierjg001@users.noreply.github.com>
2022-05-21 12:31:03 +0200

    Subida security config bueno

---
+++ PractFinal/src/main/java/com/Spring/PractFinal/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.Spring.PractFinal.config;
+
+import com.Spring.PractFinal.model.CustomerModel;
+import com.Spring.PractFinal.repository.CustomerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  CustomerRepository repository;
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeRequests().antMatchers("/inicio.html","/login.html","/registro.html").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+    .loginPage("/login.html").permitAll()
+    .defaultSuccessUrl("/precioDia.html");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		final PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    Iterable<CustomerModel> listaUsuarios=repository.findAll();
+    for (CustomerModel i:listaUsuarios){
+      auth.inMemoryAuthentication()
+      .withUser(i.getCustomerName())
+      .password(encoder.encode(i.getPassword()))
+      .roles("CUSTOMER"); 
+      }
+   }
+
+}
+
